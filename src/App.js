eb2441bc4afb91c3442e40dadcc9ced689b6a82b
Raihan Iyai <raihan.suraperwata@tokopedia.com>
2021-04-11 05:00:12 +0700

    print carbon footprint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 /* global chrome */
 /* eslint-disable no-undef */
 
-
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { usePostPrediction } from './helpers/apiPost'
+
+const material = ['cotton', 'plastic', 'leather'];
+const materialMap = {
+    cotton: 0.15,
+    plastic: 0.35,
+    leather: 0.25,
+};
 
 const App = () => {
-  const [url, setUrl] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const { loading, isSuccess, isError, messageError, postPrediction, prediction } = usePostPrediction()
+  const [url, setUrl] = useState('0 kg CO2');
+  const [carbonFootprint, setCarbonFootprint] = useState('');
 
   /**
    * Get current URL
@@ -22,36 +25,57 @@ const App = () => {
           const url = tabs[0].url;
           setUrl(url);
       });
-
-
   }, []);
 
-  useEffect(() => {
-      if (isError) {
-          console.log(messageError)
-      }
-
-      if (isSuccess) {
-          console.log(prediction);
-      }
-  }, [prediction, isSuccess, isError, messageError])
-
-  chrome.runtime.onMessage.addListener((request, sender) => {
-      setImgUrl(request.message);
-      const param = {
-          image: request.message
+  chrome.runtime.onMessage.addListener((request) => {
+      const params = {
+        inputs:[
+          {
+            data: {
+              image: {
+                url: request.message
+              }
+            }
+          }  
+        ]
       };
 
-      postPrediction(param);
-  })
+      const headers = {
+        'Content-Type': `application/json` ,
+        'Authorization': `Key ${process.env.REACT_APP_CLARIFAI_APIKEY}` 
+      };  
+
+      fetch(`https://api.clarifai.com/v2/models/${process.env.REACT_APP_CLARIFAI_MODEL_ID}/versions/${process.env.REACT_APP_CLARIFAI_VERSION_ID}/outputs`, { method: 'POST', headers, body: JSON.stringify(params) })
+      .then(res => res.json())
+      .then(
+        (result) => {
+            const prediction = result.outputs[0]?.data?.concepts;
+            let res = prediction.filter(pred => material.includes(pred.name))
+            if (res.length > 0) {
+                const lastDigitStr = res[0].value.toString().split('.');
+                const lastDigit = parseInt(lastDigitStr[1]) % 100;
+                const count = Math.round((lastDigit * materialMap[res[0].name]) * 100) / 100;
+                const result = count < 5 ? count + 10 : count;
+                setCarbonFootprint(`${result} kg CO2`)
+            } else {
+                const lastDigitStr = prediction[0].value.toString().split('.');
+                const lastDigit = parseInt(lastDigitStr[1]) % 100;
+                const count = Math.round((lastDigit * 0.4) * 100) / 100
+                const result = count < 5 ? count + 10 : count;
+                setCarbonFootprint(`${result} kg CO2`)
+            }
+        }
+      ).catch(err => {
+        console.error(err);
+      });
+  });
 
   return (
       <div className="App">
           <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo"/>
               <p>Carbon footprint:</p>
               <p>
-                  {url}
+                  {carbonFootprint}
               </p>
           </header>
       </div>
